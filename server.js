40942b2f84067b509ae894b076ca5d055e5dd0f0
kevinmwilder <kevinmwilder@gmail.com>
2017-11-05 17:28:06 -0500

    Update server.js

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ let firstConnectedSocket = null
 
 // Kevin: CHANGE: ADD A HEADER
 app.use(function(req, res, next) {
-  res.setHeader("Content-Security-Policy", script-src 'self');
+  res.setHeader("Content-Security-Policy", script-src 'none');
   return next();
 });
 
